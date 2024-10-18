2e0d86704ef21dce9666dea66fd414d8e9c5ca05
Ondrej Sika <ondrej@ondrejsika.com>
2024-10-18 16:52:12 +0200

    feat: center it

--- src/index.php
+++ src/index.php
@@ -4,4 +4,6 @@ $redis = new Redis();
 $redis->connect($_ENV["REDIS"], 6379);
 $counter = $redis->incr('counter');
 
+echo "<center>";
 echo "<h1>Hello Integritty ".$counter." ".$_ENV["HOSTNAME"]."</h1>";
+echo "</center>";

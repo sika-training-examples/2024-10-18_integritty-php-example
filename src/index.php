27f1f54d3df9dc201233ba1917b8784e71147f18
Ondrej Sika <ondrej@ondrejsika.com>
2024-10-18 10:30:15 +0200

    feat: Add counter with redis backend

--- src/index.php
+++ src/index.php
@@ -1,5 +1,9 @@
 <?php
 
-echo "<h1>Hello Integritty</h1>";
+$redis = new Redis();
+$redis->connect('redis', 6379);
+$counter = $redis->incr('counter');
+
+echo "<h1>Hello Integritty ".$counter."</h1>";
 
 phpinfo();
